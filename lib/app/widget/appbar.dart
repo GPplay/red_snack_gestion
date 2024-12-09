a2ac545b42181e597e7c7e1a984f79a126cfceca
GPplay <gybramllamasp@gmail.com>
2024-12-08 21:11:49 -0500

    cambio el nombre de la pagina de inventario

--- lib/app/widget/appbar.dart
+++ lib/app/widget/appbar.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:red_snack_gestion/app/pages/home.dart';
 import 'package:red_snack_gestion/app/pages/inicio_sesion.dart';
-import 'package:red_snack_gestion/app/pages/inventario.dart';
+import 'package:red_snack_gestion/app/pages/inventario_page.dart';
 import 'package:red_snack_gestion/app/pages/historial_venta_page.dart';
 import 'package:red_snack_gestion/app/pages/usuario_page.dart';
 
