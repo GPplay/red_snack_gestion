b0e0e722fe66e7f0176e6d8f20bf64c51c35d7eb
GPplay <gybramllamasp@gmail.com>
2024-12-19 14:43:54 -0500

    refactorizando usuario

--- lib/app/pages/usuario_page.dart
+++ lib/app/pages/usuario_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:red_snack_gestion/app/pages/chat_page.dart';
 import 'package:red_snack_gestion/app/widget/appbar.dart';
-import 'package:red_snack_gestion/app/widget/correo.dart';
 import 'package:red_snack_gestion/app/widget/contrasena.dart';
+import 'package:red_snack_gestion/app/widget/correo.dart';
 
 class UsuarioPage extends StatefulWidget {
   const UsuarioPage({super.key});
@@ -12,36 +12,156 @@ class UsuarioPage extends StatefulWidget {
 }
 
 class _UsuarioPageState extends State<UsuarioPage> {
-  final bool _showPasswordFields = false;
-  final bool _showEmailFields = false;
+  bool _showPasswordFields = false;
+  bool _showEmailFields = false;
 
   final TextEditingController _oldPasswordController = TextEditingController();
   final TextEditingController _newPasswordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
-  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _newEmailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const GlobalAppBar(title: 'Usuario', chatPage: Chats()),
       drawer: const SideMenu(),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            if (_showPasswordFields)
-              PasswordFields(
-                oldPasswordController: _oldPasswordController,
-                newPasswordController: _newPasswordController,
-                confirmPasswordController: _confirmPasswordController,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              // Imagen de perfil y nombre
+              Center(
+                child: Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 50,
+                      backgroundColor: Colors.purple[100],
+                      child: const Icon(
+                        Icons.person,
+                        size: 60,
+                        color: Colors.purple,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'NOMBRE',
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            if (_showEmailFields)
-              EmailFields(
-                emailController: _emailController,
+              const SizedBox(height: 30),
+              // Información del usuario
+              Container(
+                padding: const EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                  boxShadow: [
+                    BoxShadow(
+                      // ignore: deprecated_member_use
+                      color: Colors.grey.withOpacity(0.5),
+                      blurRadius: 5,
+                      offset: const Offset(0, 3),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('ID'),
+                    const Divider(),
+                    const Text('ID Emprendimiento'),
+                    const Divider(),
+                    const Text('Correo electrónico'),
+                    const Divider(),
+                    // Botón para cambiar la contraseña
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Cambiar contraseña'),
+                        IconButton(
+                          icon: Icon(
+                            _showPasswordFields
+                                ? Icons.keyboard_arrow_up
+                                : Icons.keyboard_arrow_down,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _showPasswordFields = !_showPasswordFields;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    // Campos para cambiar la contraseña
+                    Visibility(
+                      visible: _showPasswordFields,
+                      child: PasswordFields(
+                        oldPasswordController: _oldPasswordController,
+                        newPasswordController: _newPasswordController,
+                        confirmPasswordController: _confirmPasswordController,
+                      ),
+                    ),
+                    const Divider(),
+                    const Text('Correo electrónico'),
+                    const Divider(),
+                    // Botón para actualizar el correo
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text('Actualizar correo'),
+                        IconButton(
+                          icon: Icon(
+                            _showEmailFields
+                                ? Icons.keyboard_arrow_up
+                                : Icons.keyboard_arrow_down,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _showEmailFields = !_showEmailFields;
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    // Campos para actualizar el correo
+                    Visibility(
+                      visible: _showEmailFields,
+                      child: EmailFields(
+                        emailController: _newEmailController,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            // Otros widgets y lógica de la página
-          ],
+              const SizedBox(height: 20),
+              // Botón de borrar cuenta
+              ElevatedButton(
+                onPressed: () {
+                  // Acción para borrar la cuenta
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 15, horizontal: 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Borrar cuenta',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
